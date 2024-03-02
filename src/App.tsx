04b9cb7bc654f1d8cd34fd1765ff61c4a2f7c8f8
s3xmaxman <badside3919@gmail.com>
2024-03-03 08:18:53 +0900

    Data provider setup

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,7 @@ import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
-import dataProvider, {
-  GraphQLClient,
-  liveProvider,
-} from "@refinedev/nestjs-query";
+import { dataProvider, liveProvider } from "./provider";
 import routerBindings, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
@@ -28,8 +25,8 @@ function App() {
           <AntdApp>
             <DevtoolsProvider>
               <Refine
-                // dataProvider={dataProvider(gqlClient)}
-                // liveProvider={liveProvider(wsClient)}
+                dataProvider={dataProvider}
+                liveProvider={liveProvider}
                 notificationProvider={useNotificationProvider}
                 routerProvider={routerBindings}
                 // authProvider={}
