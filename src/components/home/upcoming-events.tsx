fed5eea02521d5e7b043cf82c833222a0406f524
s3xmaxman <badside3919@gmail.com>
2024-03-05 10:03:14 +0900

    add upcoming list

--- src/components/home/upcoming-events.tsx
+++ src/components/home/upcoming-events.tsx
@@ -1,45 +1,94 @@
 import { CalendarOutlined } from '@ant-design/icons'
-import { Card, List } from 'antd'
-import React, { useState } from 'react'
+import { Badge, Card, List } from 'antd'
 import { Text } from '../text'
+import UpcomingEventsSkeleton from '../skeleton/upcoming-events';
+import { getDate } from '@/utilities/helpers';
+import { useList } from '@refinedev/core';
+import { DASHBORAD_CALENDAR_UPCOMING_EVENTS_QUERY } from '@/graphql/queries';
+import dayjs from 'dayjs';
 
 const UpcomingEvents = () => {
-  const [isLoading, setIsLoading] = useState(true)
+  const { data, isLoading } = useList({
+    resource:  'events',
+    pagination: { pageSize: 5},
+    sorters: [
+      {
+        field: 'startDate',
+        order: 'asc'
+      }
+    ],
+    filters: [
+      {
+        field: 'startDate',
+        operator: 'gte',
+        value: dayjs().format('YYYY-MM-DD')
+      }
+    ],
+    meta: {
+      gqlQuery: DASHBORAD_CALENDAR_UPCOMING_EVENTS_QUERY
+    }
+  });
+
   return (
-    <Card
-        style={{ height: '100%' }}
-        headStyle={{ padding: '8px 16px' }}
-        bodyStyle={{ padding: '0 1rem' }}
-        title={
-            <div
-                style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    gap: '8px',
-                }}
-            >
-                <CalendarOutlined />
-                <Text size='sm' style={{ marginLeft: '0.7rem' }}>
-                    今後のイベント
-                </Text>
-            </div>
-        }
+    <Card 
+      style={{ height: '100%'}} 
+      headStyle={{padding: '8px 16px' }} 
+      bodyStyle={{ padding: '0 1rem' }}
+      title={
+        <div style={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: '8px',
+        }}>
+          <CalendarOutlined />
+          <Text size="sm" style={{ marginLeft: "0.7rem" }}>
+            今後のイベント
+          </Text>
+        </div>
+      }
     >
-        {isLoading ? (
-            <List
-                itemLayout='horizontal'
-                dataSource={Array.from({ length: 5 }).map((_, index) => ({
-                    id:index,
-                    
-                }))}
+      {isLoading ? (
+        <List
+          itemLayout="horizontal"
+          dataSource={Array.from({ length: 5 }).map((_, index) => ({
+            id: index,
+          }))}
+          renderItem={() => <UpcomingEventsSkeleton />}
+        />
+      ) : (
+        <List
+          itemLayout='horizontal'
+          dataSource={data?.data || []} 
+          renderItem={(item) => {
+            const renderDate = getDate(item.startDate, item.endDate)
+
+            return (
+              <List.Item>
+                <List.Item.Meta 
+                  avatar={<Badge color={item.color} />}
+                  title={<Text size="xs">{renderDate}</Text>}
+                  description={<Text ellipsis={{ tooltip: true }} strong>
+                    {item.title}
+                  </Text>}
+                />
+              </List.Item>
+            )
+          }}
+        />
+      )}
+
+        {!isLoading && data?.data.length === 0 && (
+            <span
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '220px'
+              }}
             >
-
-            </List>
-        ): (
-            <List>
-
-            </List>
-        )}
+              開催予定のイベントはありません
+            </span>
+          )}
     </Card>
   )
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,19 @@
 import UpcomingEvents from "./home/upcoming-events";
 import DealsChart from "./home/deals-chart";
 
-export { UpcomingEvents, DealsChart };
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+import AccordionHeaderSkeleton from "./skeleton/accordion-header";
+import KanbanColumnSkeleton from "./skeleton/kanban";
+import ProjectCardSkeleton from "./skeleton/project-card";
+import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+
+export { 
+    UpcomingEvents, 
+    DealsChart, 
+    
+    UpcomingEventsSkeleton,
+    AccordionHeaderSkeleton,
+    KanbanColumnSkeleton,
+    ProjectCardSkeleton,
+    LatestActivitiesSkeleton 
+};
