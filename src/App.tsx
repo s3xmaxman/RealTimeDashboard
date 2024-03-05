6bbb018de1a09399cd94f8115b089870e4785d22
s3xmaxman <badside3919@gmail.com>
2024-03-05 16:17:01 +0900

    add company list page

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,10 @@ import { App as AntdApp } from "antd";
 import { createClient } from "graphql-ws";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import { authProvider } from "./provider/auth";
-import { Home, ForgotPassword, Login, Register  } from "./pages";
+import { Home, ForgotPassword, Login, Register, CompanyList  } from "./pages";
 import Layout from "./components/layout";
 import { resources } from "./config/resources";
+import Create from "./pages/company/create";
 
 
 
@@ -59,6 +60,10 @@ function App() {
                     </Authenticated>
                     }>
                       <Route index element={<Home />} />
+                      <Route path="/companies">
+                        <Route index element={<CompanyList />} />
+                        <Route path="new" element={<Create />} />
+                      </Route>
                   </Route>
                 </Routes>
                 <RefineKbar />
