661099e579ad23ac4ea7ac8695cb154cc891c239
s3xmaxman <badside3919@gmail.com>
2024-03-06 17:36:44 +0900

    add edit page

---
+++ src/pages/company/edit.tsx
@@ -0,0 +1,100 @@
+import { Col, Form, Input, InputNumber, Row, Select } from 'antd'
+import { Edit, useForm, useSelect } from '@refinedev/antd'
+import { UPDATE_COMPANY_MUTATION } from '@/graphql/mutations';
+import CustomAvatar from '@/components/custom-avatar';
+import { getNameInitials } from '@/utilities';
+import { GetFieldsFromList } from '@refinedev/nestjs-query';
+import { UsersSelectQuery } from '@/graphql/types';
+import { USERS_SELECT_QUERY } from '@/graphql/queries';
+import SelectOptionWithAvatar from '@/components/select-option-with-avatar';
+import { businessTypeOptions, companySizeOptions, industryOptions } from '@/constants';
+import { CompanyContactsTable } from './contacts-table';
+
+const EditPage = () => {
+  const { saveButtonProps, formProps, formLoading, queryResult } = useForm({
+    redirect: false,
+    meta: {
+      gqlMutation: UPDATE_COMPANY_MUTATION
+    }
+  });
+
+  const { avatarUrl, name } = queryResult?.data?.data || {}
+
+  const { selectProps, queryResult: queryResultUsers } = useSelect<GetFieldsFromList<UsersSelectQuery>>({
+    resource: 'users',
+    optionLabel: 'name',
+    pagination: {
+      mode: 'off'
+    },
+    meta: {
+      gqlQuery: USERS_SELECT_QUERY
+    }
+  })
+
+  return (
+    <div>
+      <Row gutter={[32, 32]}>
+        <Col xs={24} xl={12}>
+          <Edit 
+            isLoading={formLoading}
+            saveButtonProps={saveButtonProps}
+            breadcrumb={false}
+          >
+            <Form {...formProps} layout='vertical'>
+              <CustomAvatar shape="square" src={avatarUrl} name={getNameInitials(name || '')} style={{width: 96, height: 96, marginBottom: '24px'}} />
+          <Form.Item
+            label="販売主"
+            name="salesOwnerId"
+            initialValue={formProps?.initialValues?.salesOwner?.id}
+          >
+            <Select 
+              placeholder="販売主を選択してください"
+              {...selectProps}
+              options={
+                queryResultUsers.data?.data.map((user) => ({
+                  value: user.id,
+                  label: (
+                    <SelectOptionWithAvatar 
+                      name={user.name}
+                      avatarUrl={user.avatarUrl ?? undefined}
+                    />
+                  )
+                })) ?? []
+              }
+            />
+          </Form.Item>
+          <Form.Item label="企業kib">
+            <Select  options={companySizeOptions} />
+          </Form.Item>
+          <Form.Item>
+            <InputNumber 
+              autoFocus
+              addonBefore='$'
+              min={0}
+              placeholder="0,00"
+            />
+          </Form.Item>
+          <Form.Item label="業界">
+            <Select options={industryOptions} />
+          </Form.Item>
+          <Form.Item label="ビジネスタイプ">
+            <Select options={businessTypeOptions} />
+          </Form.Item>
+          <Form.Item label="国" name="country">
+            <Input placeholder='国' />
+          </Form.Item>
+          <Form.Item label="ウェブサイト" name="website">
+            <Input placeholder="ウェブサイト" />
+          </Form.Item>
+            </Form>
+          </Edit>
+        </Col>
+        <Col xs={24} xl={12}>
+          <CompanyContactsTable />
+        </Col>
+      </Row>
+    </div>
+  )
+}
+
+export default EditPage
